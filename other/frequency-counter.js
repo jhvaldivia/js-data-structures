c383e9bdb793684ac4bc4a9f1723234de43aa133
Jaime Valdivia <jaivaldivia@gmail.com>
2022-07-24 17:59:51 -0500

    Frequency counter function added

---
+++ other/frequency-counter.js
@@ -0,0 +1,31 @@
+const TEST_VALUE_1 = [1,2,3,2,5];
+const TEST_VALUE_2 = [9,1,4,4,25];
+
+function isQuadraticEquivalent(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  let fc1 = {};
+  let fc2 = {};
+  for (let val of array1) {
+    fc1[val] = (fc1[val] || 0) + 1;
+  }
+  for (let val of array2) {
+    fc2[val] = (fc2[val] || 0) + 1;
+  }
+
+  for (let key in fc1) {
+    if (!(key ** 2 in fc2)) {
+      return false;
+    }
+    if (fc2[key ** 2] !== fc1[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const result = isQuadraticEquivalent(TEST_VALUE_1, TEST_VALUE_2);
+console.log('Is one array the quadratic equivalent of the other?', result);
