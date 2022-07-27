9db094f8e77040948aac501dbbbd302e9183c9de
Jaime Valdivia <jaivaldivia@gmail.com>
2022-07-26 23:03:49 -0500

    Simple bubble sort algorithm implementation

---
+++ algorithms/sort/bubble-sort.js
@@ -0,0 +1,31 @@
+/**
+ * The bubbleSort will return the same array of numbers but sorted in ascending order.
+ * Time complexity O(n^2)
+ * @param  {Number[]} array Array of elements (numbers).
+ * @return {Number[]}       Sorted array in ascending order.
+ */
+ function bubbleSort(array){
+  function swapElements(_arr, i, j) {
+    let temp = _arr[i];
+    _arr[i] = _arr[j];
+    _arr[j] = temp;
+  }
+
+  let swapped;
+  for (let i = array.length; i > 0; i--) {
+    swapped = false;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j+1]) {
+        swapElements(array, j, j+1);
+        swapped = true;
+      }
+    }
+
+    if (!swapped) break;
+  }
+
+  return array;
+}
+
+const result = bubbleSort([10,4,1,90,88]);
+console.log(`Result is: ${result}`);
