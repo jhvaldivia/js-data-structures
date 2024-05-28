8ac4a6e867dd36cc6d2307a038e8773cd23278d9
Jaime Valdivia <jaivaldivia@gmail.com>
2024-05-27 22:34:47 -0600

    quicksort added

---
+++ algorithms/sort/quicksort.js
@@ -0,0 +1,22 @@
+const quicksort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const less = [];
+  const greater = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+
+  return [...quicksort(less), pivot, ...quicksort(greater)];
+};
+
+const sorted = quicksort([10, 5, 2, 3]);
+console.log(sorted);
